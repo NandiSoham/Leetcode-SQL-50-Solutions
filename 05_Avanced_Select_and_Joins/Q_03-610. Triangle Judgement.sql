fb885c5478f21cb831eaecbd5da2b3ba0fb56784
Soham Nandi <56528719+NandiSoham@users.noreply.github.com>
2024-04-22 23:39:50 -0700

    Solved -> 610. Triangle Judgement

---
+++ 05_Avanced_Select_and_Joins/Q_03-610. Triangle Judgement.sql
@@ -0,0 +1,58 @@
+/* 
+
+Problem Link -> https://leetcode.com/problems/triangle-judgement/description/?envType=study-plan-v2&envId=top-sql-50
+
+------------------------------------------------------------- QUESTION -----------------------------------------------------------
+
+Table: Triangle
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| x           | int  |
+| y           | int  |
+| z           | int  |
++-------------+------+
+In SQL, (x, y, z) is the primary key column for this table.
+Each row of this table contains the lengths of three line segments.
+ 
+
+Report for every three line segments whether they can form a triangle.
+Return the result table in any order.
+The result format is in the following example.
+
+
+Example 1:
+
+Input: 
+Triangle table:
++----+----+----+
+| x  | y  | z  |
++----+----+----+
+| 13 | 15 | 30 |
+| 10 | 20 | 15 |
++----+----+----+
+
+Output: 
++----+----+----+----------+
+| x  | y  | z  | triangle |
++----+----+----+----------+
+| 13 | 15 | 30 | No       |
+| 10 | 20 | 15 | Yes      |
++----+----+----+----------+
+
+*/
+
+-- ----------------------------------------------------------- SOLUTION -----------------------------------------------------------
+
+SELECT 
+    x, y, z,
+    CASE
+        WHEN (x + y) <= z THEN 'No'
+        WHEN (y + z) <= x THEN 'No'
+        WHEN (x + z) <= y THEN 'No'
+        ELSE 'Yes'
+    END AS triangle
+FROM Triangle;
+
+
